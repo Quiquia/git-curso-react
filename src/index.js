82c07a34a598921c99e79223997e2c605092ecb9
Quiquia <vannebequi1994@gmail.com>
2023-06-28 18:22:47 -0500

    add path/

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/git-curso-react",
     element: <App />,
   },
   {
-    path: "/:user",
+    path: "/git-curso-react:user",
     element: <App />,
   },
 ]);
